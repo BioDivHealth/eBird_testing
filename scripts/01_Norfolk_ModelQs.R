50f756dbe4374e40224ed647699a1c6b6036455a
EliseGallois <elise.gallois94@gmail.com>
2024-03-11 16:33:49 +0000

    Elise calculates data retreival answers

--- scripts/01_Norfolk_ModelQs.R
+++ scripts/01_Norfolk_ModelQs.R
@@ -2,16 +2,73 @@
 # Elise Gallois, elise.gallois94@gmail.com
 # 11th March 2024
 
-# 1. Load libraries and data -----
+# 1. Load libraries -----
 library(tidyverse)
 
 # 2. Load data and save as RData for future use ----
-norfolk_ebird <- read_csv("~/Downloads/norfolk_ebird.csv") # too large for github
-save(norfolk_ebird, file = "data/norfolk_birds.RData")
 
+#norfolk_ebird <- read_csv("~/Downloads/norfolk_ebird.csv") # too large for github
+#save(norfolk_ebird, file = "data/norfolk_birds.RData")
+load("data/norfolk_birds.RData")
+View(norfolk_ebird)
 
-# 3. Data retreival questions ----
+# 3. Data retrieval questions ----
+#What is the total count of birds observed in Norfolk?
+names(norfolk_ebird)[names(norfolk_ebird) == "OBSERVATION COUNT"] <- "obs"
+norfolk_ebird$obs <- as.numeric(norfolk_ebird$obs)
+sum(norfolk_ebird$obs, na.rm = TRUE) # 1140423
 
+# Provide the average duration of observer effort per survey in Norfolk.
+names(norfolk_ebird)[names(norfolk_ebird) == "SAMPLING EVENT IDENTIFIER"] <- "survey"
+norfolk_ebird$survey <- as.factor(norfolk_ebird$survey)
+names(norfolk_ebird)[names(norfolk_ebird) == "DURATION MINUTES"] <- "duration"
+mean(norfolk_ebird$duration, na.rm = TRUE) #115.8326
+
+# How many surveys were conducted at Cley & Salthouse Marshes?
+sum(grepl("Cley", norfolk_ebird$LOCALITY, ignore.case = TRUE))
+
+# List all the bird species observed at Titchwell Marsh.
+sum(grepl("Titchwell", norfolk_ebird$LOCALITY, ignore.case = TRUE))
+names(norfolk_ebird)[names(norfolk_ebird) == "COMMON NAME"] <- "common_name"
+titchwell_data <- norfolk_ebird[grepl("Titchwell", norfolk_ebird$LOCALITY, ignore.case = TRUE), ]
+unique_species <- unique(titchwell_data$common_name)
+unique_species
+
+# What is the total count of Bearded Reedling observed in Norfolk?
+norfolk_ebird$common_name <- as.factor(norfolk_ebird$common_name)
+sum(norfolk_ebird$obs[norfolk_ebird$common_name == 'Bearded Reedling'], na.rm = TRUE)
+
+# How many surveys recorded sightings of the Western Marsh Harrier in Norfolk?
+marsh_harrier_surveys <- unique(norfolk_ebird$survey[norfolk_ebird$common_name == "Western Marsh Harrier"])
+length(marsh_harrier_surveys) # 1750
+
+# What is the average number of bird species observed per survey at Blakeney?
+blakeney_data <- norfolk_ebird[grepl("Blakeney", norfolk_ebird$LOCALITY, ignore.case = TRUE), ]
+unique_common_names_per_survey <- aggregate(common_name ~ survey, data = blakeney_data, FUN = function(x) length(unique(x)))
+mean(unique_common_names_per_survey$common_name)
+
+# Provide the total count of Black Headed Gull observed at NWT Holme Dunes.
+black_headed_gull_obs <- norfolk_ebird[norfolk_ebird$common_name == "Black-headed Gull" & grepl("NWT Holme Dunes", norfolk_ebird$LOCALITY, ignore.case = TRUE), ]
+sum(black_headed_gull_obs$obs, na.rm = TRUE)
+
+#How many observations were recorded at Happisburgh?
+happisburgh_data <- norfolk_ebird[grepl("Happisburgh", norfolk_ebird$LOCALITY, ignore.case = TRUE), ]
+nrow(happisburgh_data)
+
+#List all the exotic bird species.
+names(norfolk_ebird)[names(norfolk_ebird) == "EXOTIC CODE"] <- "exotic"
+exotic_data <- norfolk_ebird[!is.na(norfolk_ebird$exotic), ]
+nrow(exotic_data)
+
+# Which is the least common bird observed?
+obs_per_common_name <- aggregate(obs ~ common_name, data = norfolk_ebird, FUN = sum)
+lowest_obs_common_name <- obs_per_common_name[which.min(obs_per_common_name$obs), "common_name"]
+lowest_obs_common_name_df <- data.frame(common_name = lowest_obs_common_name)
+
+# Which is the most common bird observed?
+obs_per_common_name <- aggregate(obs ~ common_name, data = norfolk_ebird, FUN = sum)
+highest_obs_common_name <- obs_per_common_name[which.max(obs_per_common_name$obs), "common_name"]
+highest_obs_common_name_df <- data.frame(common_name = highest_obs_common_name)
 
 # 4. Easy inference questions -----
 
