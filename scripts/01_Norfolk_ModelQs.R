9964b83a7cb29a0e0b6fa54c5f695913a61b81bf
EliseGallois <elise.gallois94@gmail.com>
2024-03-11 15:23:30 +0000

    Elise loads in data from large download

---
+++ scripts/01_Norfolk_ModelQs.R
@@ -0,0 +1,19 @@
+# Testing questions - getting correct answers for question answering
+# Elise Gallois, elise.gallois94@gmail.com
+# 11th March 2024
+
+# 1. Load libraries and data -----
+library(tidyverse)
+
+# 2. Load data and save as RData for future use ----
+norfolk_ebird <- read_csv("~/Downloads/norfolk_ebird.csv") # too large for github
+save(norfolk_ebird, file = "data/norfolk_birds.RData")
+
+
+# 3. Data retreival questions ----
+
+
+# 4. Easy inference questions -----
+
+
+# 5. Hard inference questions -----
