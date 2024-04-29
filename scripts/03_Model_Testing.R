e20fec19c17f4f3fa5e2027b395d59a7dd299cc8
EliseGallois <elise.gallois94@gmail.com>
2024-04-29 16:40:52 +0100

    Elise adds smily face

--- scripts/03_Model_Testing.R
+++ scripts/03_Model_Testing.R
@@ -1,6 +1,7 @@
 # Initial comparison of three different models with iterative improvements
 # Elise Gallois, elise.gallois94@gmail.com
-# 29th April 2023 
+# 29th April 2024 
+# :)
 
 # 1. Load libraries -----
 library(tidyverse)
