60eca5dfc1694b6152a9d924c74efe53373af9f4
EliseGallois <elise.gallois94@gmail.com>
2024-05-29 12:24:54 +0100

    Elise creates new norfolk habitat dataframe

--- scripts/04_Habitat_Qs.R
+++ scripts/04_Habitat_Qs.R
@@ -6,6 +6,7 @@
 library(tidyverse)
 library(terra)
 library(raster)
+library(readxl)
 
 # 2. Load data and save as RData for future use ----
 
@@ -63,6 +64,48 @@ plot(uk_clipped)
 # save smaller file for easy loading
 output_file <- "data/UK_clipped_raster.tif"
 writeRaster(uk_clipped, filename = output_file, filetype = "GTiff", overwrite = TRUE)
+
+# 3. Extract habitat types from TIF ----
+# bind coords from ebird data
+coordinates <- cbind(norfolk_ebird$LONGITUDE, norfolk_ebird$LATITUDE)
+points <- vect(coordinates, crs = crs(uk_clipped))
+
+# extract values from uk_clipped IUCN tif
+extracted_values <- extract(uk_clipped, points)
+
+# create 'newcode' column within norfolk_ebird so I can later bind these codes to the key
+norfolk_habitat$NewCode <- extracted_values[, 2]  
+
+# import key 
+iucn_key <- read_excel("data/IUCN_mapping_legend.xlsx")
+
+# Ensure "NewCode" columns are of the same type
+norfolk_habitat$NewCode <- as.character(norfolk_habitat$NewCode)
+iucn_key$NewCode <- as.character(iucn_key$NewCode)
+
+# Merge norfolk_ebird with iucn_key on "NewCode"
+merged_data <- merge(norfolk_habitat, iucn_key, by = "NewCode", all.x = TRUE)
+
+# Rename the "IUCNLevel" column to "Habitat" in the merged dataframe
+merged_data <- rename(merged_data, Habitat = IUCNLevel)
+
+# If you want to keep only the necessary columns, you can select them
+norfolk_habitat <- merged_data[, c(names(norfolk_habitat), "Habitat")]
+
+# Check the result
+head(norfolk_habitat)
+
+# drop species column
+norfolk_habitat <- norfolk_habitat %>% dplyr::select(-Species)
+norfolk_habitat <- norfolk_habitat %>% dplyr::select(-NewCode)
+
+# rename columns in a bid to help LLM understanding
+norfolk_habitat <- norfolk_habitat %>%
+  rename(HabitatRange = HabitatTypes,
+         ActualHabitat = Habitat)
+
+# Write the dataframe to a CSV file
+write.csv(norfolk_habitat, "data/norfolk_habitat.csv", row.names = FALSE)
 
 
 
