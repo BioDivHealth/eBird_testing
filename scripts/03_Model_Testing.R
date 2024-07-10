e19f15cd11bb9ded71cbaffa3ecd0b05fbd2cc83
EliseGallois <elise.gallois94@gmail.com>
2024-07-10 11:45:20 +0100

    Elise updates qual test plot

--- scripts/03_Model_Testing.R
+++ scripts/03_Model_Testing.R
@@ -9,13 +9,14 @@ library(tidyverse)
 library(esquisse) 
 library(viridis)
 
+
 # 2. Load data  ----
 model_testing <- read_csv("data/model_testing.csv")
 
 # 3. Plot comparisons ----
 
 # 1 - Simple truth breakdown 
-ggplot(model_testing) +
+(basic_comparison <- ggplot(model_testing) +
   aes(x = quality_scoring, fill = quality_scoring) +
   geom_bar() +
   scale_fill_manual(
@@ -24,7 +25,7 @@ ggplot(model_testing) +
                Wrong = "#D94714")
   ) +
   theme_classic() +
-  facet_wrap(vars(model))
+  facet_wrap(vars(model)))
 
 # 2 - By Query Type
 # Define the new order and names for the levels
@@ -34,14 +35,20 @@ model_testing$model <- recode(model_testing$model, matrix_only = '1: No prompt',
                         metadata_prompt_certain = '4: Prompt: Thorough Metadata with Examples',
                         CAMB_metadata_prompt_certain = '5: Prompt: Different county' )
 
+
 ggplot(model_testing) +
   aes(x = quality_scoring, fill = query_type) +
   geom_bar() +
+  labs(x = "Quality Scoring",
+       y = "Count",
+       legend_title = "Query Type") +
   scale_fill_viridis_d(option = "plasma", direction = 1) +
   theme_classic() +
-  facet_wrap(vars(model))
+  facet_wrap(vars(model), scales='free')
 
- # 3 - Time-specific
+
+
+# 3 - Time-specific
 model_testing %>%
   filter(time_spec %in% "S") %>%
   ggplot() +
@@ -49,7 +56,6 @@ model_testing %>%
   geom_bar(fill = "#EBBB06") +
   theme_classic() +
   facet_wrap(vars(model))
-
 
 
 
